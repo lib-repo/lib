530bda616b231017210ab4406bfb8c30c1b76791
박민서 <qkrals2475@naver.com>
2025-02-07 19:38:52 +0900

    fix: 대출 상태 수정

--- src/main/java/org/example/libdev/rent/entity/RentStatus.java
+++ src/main/java/org/example/libdev/rent/entity/RentStatus.java
@@ -2,10 +2,8 @@ package org.example.libdev.rent.entity;
 
 public enum RentStatus {
 
-        PENDING,    // 신청중
         RENTED,      // 대여중
         RETURNED,    // 반납 완료
-        OVERDUE,     // 연체됨
-        CANCELED     // 취소됨
+        OVERDUE     // 연체됨
 
 }
