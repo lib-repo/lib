f5e5f31a7ac37420439e4570371e964b6108ffc4
[CLOUD5] 이춘우 <dlcnsdncjswo@naver.com>
2025-02-15 03:55:32 +0000

    Update LibDevApplication.java

--- src/main/java/org/example/libdev/LibDevApplication.java
+++ src/main/java/org/example/libdev/LibDevApplication.java
@@ -4,16 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @SpringBootApplication
 public class LibDevApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibDevApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
     }
 
 }
