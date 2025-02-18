ec9c3434f8e8946f8ab1f104d607cb12ba3a088a
[CLOUD5] 이춘우 <dlcnsdncjswo@naver.com>
2025-02-18 01:08:58 +0000

    Update BaseEntity.java

--- src/main/java/org/example/libdev/global/entity/BaseEntity.java
+++ src/main/java/org/example/libdev/global/entity/BaseEntity.java
@@ -14,11 +14,10 @@ import java.time.LocalDateTime;
 public class BaseEntity {
 
     @CreatedDate
-    @Column(updatable = false, nullable = false)
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(nullable = false)
     private LocalDateTime updatedAt;
 
     @Column
